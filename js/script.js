2ad834250594338b71a00866c47aa369c7213bcb
ryoko-y <wagarden6@gmail.com>
2021-06-28 12:26:48 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+var footerColor = document.querySelector("footer");
+//console.log(footerColor);
+footerColor.style.backgroundColor = "#d7ddf3";
+
+var tagline = document.querySelector(".tagline");
+//console.log(tagline);
+tagline.innerText = "surprise your significant special, just because...";
+
+var address = document.querySelector("address");
+//console.log(address);
+address.style.lineHeight = "1.5em";
+address.style.fontSize = "1.75em";
+
+var emphasize = document.querySelector("h3");
+//console.log(emphasize);
+emphasize.innerHTML = "We're here for you <em>every day</em> of the week.";
+
+var highlight = document.querySelector("em");
+//console.log(highlight);
+highlight.style.color = "#fe8181";
+
+var insta = document.querySelector(".fa-instagram");
+insta.style.color = "#d45bb9";
+
+var fb = document.querySelector(".fa-facebook");
+fb.style.color = "#6a00b5";
+
+var hours = document.querySelector(".contact p");
+//console.log(hours);
+hours.style.border = "1px solid #d45bb9";
+hours.style.padding = "1em";
